3fed2485d77d9c728b9a6d69cc3558437676524a
wohlfea <wohlfea@gmail.com>
2015-11-04 14:58:31 -0800

    Added logic to add new branches to the list

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+var newStoreCounter = 1;
 var table = document.getElementById('table');
 var topRow = document.getElementById('topRow');
 var hoursArray = ['10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
-
+var form = document.getElementById('theForm');
 function Store (minCustHr, maxCustHr, avgCookiesHr, storeName) {
   this.minCustHr =  minCustHr;
   this.maxCustHr = maxCustHr;
@@ -63,9 +64,31 @@ function makeTopRow (){
     }
   }
 }
+function createStore() {
+window['newStore'+newStoreCounter] = new Store(event.target.minCustInput.value, event.target.maxCustInput.value, event.target.avgPerCustInput.value, event.target.locationInput.value);
+window['newStore'+newStoreCounter].theLoop();
+event.target.minCustInput.value = null;
+event.target.maxCustInput.value = null;
+event.target.avgPerCustInput.value = null;
+event.target.locationInput.value = null;
+newStoreCounter++;
+}
+function validateCreate(event){
+  event.preventDefault();
+  console.log('you clicked the button');
+  if (!event.target.locationInput.value || !event.target.minCustInput.value || !event.target.maxCustInput.value || !event.target.avgPerCustInput.value) {
+    return alert('Fields cannot be empty!');
+  } else if (isNaN(event.target.minCustInput.value) || isNaN(event.target.maxCustInput.value) || isNaN(event.target.avgPerCustInput.value)) {
+    return alert('You entered a letter in a number field!');
+  } else {
+    console.log('User entered Valid Input');
+    createStore();
+  };
+}
 makeTopRow();
 pikePlace.theLoop();
 seaTac.theLoop();
 southCenter.theLoop();
 bellSquare.theLoop();
 alki.theLoop();
+form.addEventListener('submit', validateCreate);
